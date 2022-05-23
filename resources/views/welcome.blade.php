ea8f7a2e4d435c2e05653b19780cd80599266995
francop16 <desarrolloswebjunin@gmail.com>
2022-05-23 12:34:45 -0300

    secure asset app js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,10 +9,10 @@
     
         <title>{{ config('app.name', 'Laravel') }}</title>
     
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-        <!-- Scripts para heroku
-        <script src="{{ secure_asset('js/app.js') }}" defer></script> -->
+        <!-- Scripts 
+        <script src="{{ asset('js/app.js') }}" defer></script>-->
+        <!-- Scripts para heroku-->
+        <script src="{{ secure_asset('js/app.js') }}" defer></script> 
     
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
